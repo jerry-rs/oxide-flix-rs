3bd37f3dda6e2938781a21d1bfcd44f984609482
dsy <dsy@MacBook-Pro-M1.local>
2026-07-14 16:10:31 +0800

    changed:index.html style

--- src/vides/handlers/delete.rs
+++ src/vides/handlers/delete.rs
@@ -9,8 +9,8 @@ pub(crate) async fn video_delete_handler(
     axum::extract::Path(delete_path): axum::extract::Path<String>,
 ) -> Response {
     if delete_path.trim().is_empty() {
-        error!("{delete_path}是根目录不允许删除");
-        return (StatusCode::BAD_REQUEST, "不允许删除根目录").into_response();
+        error!("{delete_path} is root path and not delete");
+        return (StatusCode::BAD_REQUEST, "not delete root path").into_response();
     }
     let safe_path = match std::fs::canonicalize(state.config.data_dir.join(&delete_path)) {
         Ok(path) => path,
@@ -18,30 +18,30 @@ pub(crate) async fn video_delete_handler(
             error!("{e}");
             return (
                 StatusCode::NOT_FOUND,
-                format!("{}文件或文件夹不存在", delete_path),
+                format!("{} file or folder not exist", delete_path),
             )
                 .into_response();
         }
     };
     if safe_path.is_dir() {
         if let Err(e) = tokio::fs::remove_dir_all(&safe_path).await {
-            error!("失败删除文件夹 {} :{e}", &safe_path.display());
+            error!("failed to delete {} :{e}", &safe_path.display());
             return (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                format!("失败删除文件夹 {}", delete_path),
+                format!("failed to delete {}", delete_path),
             )
                 .into_response();
         }
     } else {
         if let Err(e) = tokio::fs::remove_file(&safe_path).await {
-            error!("失败删除文件 {} :{e}", &safe_path.display());
+            error!("failed to delete {} :{e}", &safe_path.display());
             return (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                format!("失败删除文件 {}", delete_path),
+                format!("failed to delete {}", delete_path),
             )
                 .into_response();
         }
     };
-    info!("成功删除{}",safe_path.display());
+    info!("success to delete {}", safe_path.display());
     (StatusCode::OK, "SUCCESS").into_response()
 }
